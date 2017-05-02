544930c7fd694dd3188324c1d6859c52d33fac05
msingh <manprets@hotmail.com>
2017-05-01 23:01:34 -0400

    feat: added code incase ssn are invalids

--- spark_tasks.scala
+++ spark_tasks.scala
@@ -18,7 +18,6 @@ val claimant = spark.read.option("header",true).csv("./Documents/spark/claimant_
 
 val claimant = spark.read.option("header",true).option("inferSchema","true").csv("./Documents/spark/claimant_data.csv").toDF
 
-
 claimant.groupBy($"uniq").count.show
 
 
@@ -42,5 +41,17 @@ val claimant_kv = claimant.groupBy("ssn").min("uniq").withColumnRenamed("min(uni
 
 val joined = claimant.join(claimant_kv,'ssn_uniq==='ssn,"inner").select("uniq","dim","ssn","min_uniq")
 
+joined.select("uniq","min_uniq").distinct().orderBy("uniq").show()
 
 
+
+val claimant_new = spark.read.option("header",true).csv("./Documents/spark/claimant_data_new.csv").toDF
+val claimant_new = spark.read.option("header",true).option("inferSchema","true").csv("./Documents/spark/claimant_data_new.csv").toDF
+
+val claimant_new_kv = claimant_new.groupBy("ssn").min("uniq").withColumnRenamed("min(uniq)", "min_uniq").withColumnRenamed("ssn","ssn_uniq")
+claimant_new_kv.filter($"ssn_uniq"< "'U'").show()
+val claimant_new_kv = claimant_new.groupBy("ssn").min("uniq").withColumnRenamed("min(uniq)", "min_uniq").withColumnRenamed("ssn","ssn_uniq").filter($"ssn_uniq"< "'U'")
+
+val joined_new = claimant_new.join(claimant_new_kv,'ssn_uniq==='ssn,"inner").select("uniq","dim","ssn","min_uniq")
+
+joined_new.select("uniq","min_uniq").distinct().orderBy("uniq").show()
